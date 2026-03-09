fa9edc59b38f9f4084b9f0d61eb70f3fd50a37de
Sophia Rainiel Aguado <saguado@ucsc.edu>
2026-03-09 15:03:38 -0700

    Reformmating & attempt fix
    + Attempting to fix pages error for phaser file
    + Reformmated code so all the litter minigame could is together

--- src/scenes/PostcardFront.js
+++ src/scenes/PostcardFront.js
@@ -15,15 +15,8 @@ class Front extends Phaser.Scene {
         this.trashcan = this.physics.add.image(50, game.config.height / 3 + 50, "trashcan")
             .setOrigin(0).setScale(0.5).setInteractive()
 
-        // Running Minigame
-        this.duo = this.add.image(game.config.width - 200, game.config.height / 2 + 100, "duoCutout").setOrigin(0).setScale(0.5)
-            .setInteractive({ useHandCursor: true })
-            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-                this.scene.start("dialougeScene");
-            })
-
-
         this.trashGroup = this.physics.add.group()
+
         //SPAWN FUCTIONS
         for (var i = 0; i < 6; i++) {
             let trash = new Litter(this, Phaser.Math.Between(100, 725),
@@ -32,6 +25,14 @@ class Front extends Phaser.Scene {
             this.input.setDraggable(trash)
             this.trashGroup.add(trash)
         }
+        
+        // Running Minigame
+        this.duo = this.add.image(game.config.width - 200, game.config.height / 2 + 100, "duoCutout").setOrigin(0).setScale(0.5)
+            .setInteractive({ useHandCursor: true })
+            .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
+                this.scene.start("dialougeScene");
+            })
+
 
         cursors = this.input.keyboard.createCursorKeys()
 
