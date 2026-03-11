d238cda3f89766c915011e5ad42af96f4ab1baa4
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-03-10 23:11:42 -0700

    [feat] Litter Static Movement
    + Litter bobs when not being clicked and dragged by cursor

--- src/prefabs/Litter.js
+++ src/prefabs/Litter.js
@@ -1,12 +1,21 @@
-class Litter extends Phaser.Physics.Arcade.Sprite{
-    constructor(scene, x, y, texture, frame){
+class Litter extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame)
 
         this.scene.add.existing(this)
         this.scene.physics.add.existing(this)
 
-        this.setInteractive({draggable: true})
+        this.setInteractive({ draggable: true })
         this.body.onCollide = true
         this.body.onOverlap = true
+
+        this.floatTween = this.scene.tweens.add({
+            targets: this,
+            y: this.y + Phaser.Math.Between(3, 7),
+            duration: Phaser.Math.Between(700, 1200),
+            ease: "Sine.easeInOut",
+            yoyo: true,
+            repeat: -1
+        });
     }
 }
--- src/scenes/PostcardFront.js
+++ src/scenes/PostcardFront.js
@@ -15,6 +15,30 @@ class Front extends Phaser.Scene {
         this.trashcan = this.physics.add.image(50, game.config.height / 3 + 50, "trashcan")
             .setOrigin(0).setScale(0.5).setInteractive()
 
+        this.input.on("dragstart", (pointer, gameObject) => {
+            if (gameObject.floatTween) {
+                gameObject.floatTween.pause()
+            }
+        })
+        this.input.on("dragstart", (pointer, gameObject) => {
+            if (gameObject.floatTween) {
+                gameObject.floatTween.stop();
+            }
+        });
+
+        this.input.on("dragend", (pointer, gameObject) => {
+
+            // restart float animation
+            gameObject.floatTween = this.tweens.add({
+                targets: gameObject,
+                y: gameObject.y + Phaser.Math.Between(3, 7),
+                duration: Phaser.Math.Between(700, 1200),
+                ease: "Sine.easeInOut",
+                yoyo: true,
+                repeat: -1
+            });
+
+        });
         // Running Minigame
         this.duo = this.add.image(game.config.width - 200, game.config.height / 2 + 100, "duoCutout")
             .setOrigin(0).setScale(0.5)
@@ -29,13 +53,13 @@ class Front extends Phaser.Scene {
         //SPAWN FUCTIONS
         for (var i = 0; i < 6; i++) {
             let trash = new Litter(this, Phaser.Math.Between(100, 725),
-                Phaser.Math.Between(game.config.height / 2 - 50, game.config.height / 2 + 200), 
+                Phaser.Math.Between(game.config.height / 2 - 50, game.config.height / 2 + 200),
                 "trash", Phaser.Math.Between(0, 4)); // Random frame
 
             this.input.setDraggable(trash)
             this.trashGroup.add(trash)
         }
-        
+
         // Running Minigame
         this.duo = this.add.image(game.config.width - 200, game.config.height / 2 + 100, "duoCutout").setOrigin(0).setScale(0.5)
             .setInteractive({ useHandCursor: true })
