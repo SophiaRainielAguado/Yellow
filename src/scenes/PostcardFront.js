bbd0b9d223cab4ae4f489ee5b77e7904ae55499f
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-03-10 20:13:47 -0700

    [refractor] Trash spritesheet: Replaced temp_trash.png with trash.png. Randomized sprite at spawn for Litter prefab

--- src/scenes/PostcardFront.js
+++ src/scenes/PostcardFront.js
@@ -16,7 +16,8 @@ class Front extends Phaser.Scene {
             .setOrigin(0).setScale(0.5).setInteractive()
 
         // Running Minigame
-        this.duo = this.add.image(game.config.width - 200, game.config.height / 2 + 100, "duoCutout").setOrigin(0).setScale(0.5)
+        this.duo = this.add.image(game.config.width - 200, game.config.height / 2 + 100, "duoCutout")
+            .setOrigin(0).setScale(0.5)
             .setInteractive({ useHandCursor: true })
             .on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
                 this.scene.start("dialougeScene");
@@ -24,10 +25,12 @@ class Front extends Phaser.Scene {
 
 
         this.trashGroup = this.physics.add.group()
+
         //SPAWN FUCTIONS
         for (var i = 0; i < 6; i++) {
             let trash = new Litter(this, Phaser.Math.Between(100, 725),
-                Phaser.Math.Between(game.config.height / 2 - 50, game.config.height / 2 + 200), "trash");
+                Phaser.Math.Between(game.config.height / 2 - 50, game.config.height / 2 + 200), 
+                "trash", Phaser.Math.Between(0, 4)); // Random frame
 
             this.input.setDraggable(trash)
             this.trashGroup.add(trash)
@@ -49,9 +52,5 @@ class Front extends Phaser.Scene {
             }
 
         })
-    }
-
-    update() {
-        //Add litter inteaction with trashcan, delete object
     }
 }
