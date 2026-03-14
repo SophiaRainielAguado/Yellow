32b3820b49447c275fafc89792941c5cf29987d3
Sophia Rainiel Aguado <saguado@ucsc.edu>
2026-03-13 17:21:40 -0700

    [feat] Race Minigame
    + Player speed is based on click speed
    + Random x_pos advancment for 2 npc
    + win state based on who reaches the finish line first

--- src/scenes/Race.js
+++ src/scenes/Race.js
@@ -2,17 +2,83 @@ class Race extends Phaser.Scene {
     constructor() {
         super("raceScene")
     }
-
-    // Create cursor for clicker
-    // Base speed off clicks per second?
-    // Goal based on distance?
-    // Me prefab probably
-    // Duo prefab also to keep track
-    
     create() {
         console.log("Race Minigame")
 
         document.getElementById('info').innerHTML =
-            '<strong>Race.js</strong><br>'
+            '<strong>Race.js</strong><br>CLICK!!!!<br>'
+
+        this.add.bitmapText(game.config.width / 2 - 150, 85, "crayon_font", "CLICK 4 SPEED", 45)
+        // GOAL distance! "win" variable in x-position
+        this.x_finishLine = 725
+
+        // TEMPORARY SCALE TILL ASSETS FOR RUNNING MADE
+        this.jamie = this.add.sprite(100, game.config.height / 2, "jamie").setOrigin(0).setScale(0.5)
+        this.me = this.add.sprite(100, game.config.height / 3, "me").setOrigin(0).setScale(0.5)
+        this.mike = this.add.sprite(100, game.config.height / 4 - 55, "mike").setOrigin(0).setScale(0.5)
+        // Speeds
+        this.meSpeed = 0;
+        this.jamieSpeed = Phaser.Math.Between(100, 170);
+        this.mikeSpeed = Phaser.Math.Between(100, 140);
+
+        // Click tracking
+        this.clickCount = 0;
+
+        this.input.on("pointerdown", () => {
+            this.clickCount++;
+        });
+
+        // Recalculate player speed every 0.5s
+        this.time.addEvent({
+            delay: 500,
+            loop: true,
+            callback: () => {
+                let clicksPerSecond = this.clickCount * 2;
+                this.meSpeed = clicksPerSecond * 40;
+                this.clickCount = 0;
+            }
+        });
+
+        // NPC speed randomizer
+        this.time.addEvent({
+            delay: 800,
+            loop: true,
+            callback: () => {
+                this.jamieSpeed = Phaser.Math.Between(60, 140);
+                this.mikeSpeed = Phaser.Math.Between(60, 140);
+            }
+        });
+
+        this.raceFinished = false;
+    }
+
+    update(time, delta) {
+        if (this.raceFinished) return;
+
+        const dt = delta / 1000;
+
+        // Move racers
+        this.me.x += this.meSpeed * dt;
+        this.jamie.x += this.jamieSpeed * dt;
+        this.mike.x += this.mikeSpeed * dt;
+
+        // Check finish
+        this.checkWinner();
+    }
+
+    checkWinner() {
+        if (this.me.x >= this.x_finishLine) {
+            this.endRace("me");
+        } else if (this.jamie.x >= this.x_finishLine) {
+            this.endRace("Jamie");
+        } else if (this.mikeSpeed.x >= this.x_finishLine) {
+            this.endRace("NPC 2");
+        }
+    }
+
+    endRace(winner) {
+        this.raceFinished = true;
+        console.log(`${winner} wins!`);
+        this.scene
     }
 }
